745d1fd44258e2a2f5d3e4f3680a995e6a6176fa
Mohammed Alharbi <Mohammed266433@gmail.com>
2025-07-15 20:24:05 +0300

    first commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Mohammed Alharbi on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
