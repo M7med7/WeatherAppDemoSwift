c499cc5f1aec9b269fa7bb795e0c3b7ab3953077
Mohammed Alharbi <Mohammed266433@gmail.com>
2025-07-18 18:26:01 +0300

    started on the weather page

---
+++ WeatherDemo/Extentions.swift
@@ -0,0 +1,15 @@
+//
+//  Extentions.swift
+//  WeatherDemo
+//
+//  Created by Mohammed Alharbi on 16/07/2025.
+//
+
+import Foundation
+
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
